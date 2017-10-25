5a2ff521e7967a9fecdf080b45a47511b91d5846
bushizzle <opatop@yandex.ru>
2017-10-25 05:16:02 +0400

    upd

--- source/js/custom/helpers.js
+++ source/js/custom/helpers.js
@@ -15,35 +15,6 @@ var tt_helpers = {
 			return false;
 		}
 	},
-	throttle: function(func, ms) {
-
-		var isThrottled = false,
-			savedArgs,
-			savedThis;
-
-		function wrapper() {
-
-			if (isThrottled) { // (2)
-				savedArgs = arguments;
-				savedThis = this;
-				return;
-			}
-
-			func.apply(this, arguments); // (1)
-
-			isThrottled = true;
-
-			setTimeout(function() {
-				isThrottled = false; // (3)
-				if (savedArgs) {
-					wrapper.apply(savedThis, savedArgs);
-					savedArgs = savedThis = null;
-				}
-			}, ms);
-		}
-
-		return wrapper;
-	},
 
 	get_time_string: function(sec){
 		var h = sec/3600 ^ 0,
